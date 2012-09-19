8e982f654c9b6a2578c004f2d49a5761ed288b87
Brandon R. Stoner <monokrome@limpidtech.com>
2012-09-18 21:26:33 -0600

    Updated importer to load files.

---
+++ importer/loaders.py
@@ -0,0 +1,43 @@
+import xlrd
+import os
+
+base_loader_error = 'Loader can only be used by extending it.'
+
+excel_extensions = [ '.xls', ]
+
+class Loader(object):
+    def __init__(self, file_info):
+        self.filename = file_info.path
+
+        self.open()
+
+    def open(self):
+        raise NotImplementedError(base_loader_error)
+
+    @classmethod
+    def sniff(cls, file_info):
+        raise NotImplementedError(base_loader_error)
+
+class ExcelLoader(Loader):
+    supports_sheets = True
+    type_name = 'excel'
+
+    def open(self):
+        self.backend = xlrd.open_workbook(self.filename)
+        self.sheet_names = self.backend.sheet_names()
+        self.sheet_count = len(self.sheet_names)
+
+    def sheet_by_name(self, name):
+        """ Returns a sheet based on it's name. """
+
+        return self.backend.sheet_by_name(name)
+
+    @classmethod
+    def sniff(cls, file_info):
+
+        # TODO: Find a way to really sniff the file.
+        return os.path.splitext(file_info.path)[-1] in excel_extensions
+
+# TODO: Finish Loader for importing from CSV data.
+class CSVLoader(Loader):
+    supports_sheets = False
