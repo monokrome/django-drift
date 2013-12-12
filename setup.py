f73109ae5040e216364ae1abed5b7740f1a785bc
Brandon R. Stoner <monokrome@limpidtech.com>
2013-12-11 17:58:37 -0800

    Use ez_setup, because Python is bad at packages.

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from distutils.core import setup
-
 import os
 import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from . import ez_setup
+    from setuptools import setup
+
 
 parent_directory = os.path.abspath(os.path.dirname(__file__))
 
