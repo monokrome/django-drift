ea2cab7fa39faeb1f094511da25efad866cc9dc7
Brandon R. Stoner <monokrome@limpidtech.com>
2013-12-11 20:28:41 -0800

    Fixed issue where transaction.atomic broke loggers.

--- importer/tasks.py
+++ importer/tasks.py
@@ -20,39 +20,39 @@ failure_status = 'failure'
 
 
 @celery.shared_task
-@transaction.atomic
 def importer_asynchronous_task(import_pk, *args, **kwargs):
-    logger = importer_asynchronous_task.get_logger()
+    with transaction.atomic:
+        logger = importer_asynchronous_task.get_logger()
 
-    import_instance = FileImport.objects.get(pk=import_pk)
-    ImportType = import_instance.get_related_importer(**kwargs)
+        import_instance = FileImport.objects.get(pk=import_pk)
+        ImportType = import_instance.get_related_importer(**kwargs)
 
-    if ImportType is None:
-        import_instance.status = 30
-        return False
+        if ImportType is None:
+            import_instance.status = 30
+            return False
 
-    importer = ImportType()
+        importer = ImportType()
 
-    import_instance.status = processing_status
-    import_instance.status_description = 'Currently processing file'
-    import_instance.save()
-
-    import_context = import_instance.get_context()
-
-    try:
-        if importer.process(import_context, logger) is True:
-            import_instance.status = success_status
-            import_instance.status_description = success_description
-            import_instance.save()
-
-        else:
-            raise ImportFailure(assuming_failure_message.format(
-                importer.__class__.__name__
-            ))
-
-    except ImportFailure, e:
-        import_instance.status = failure_status
-        import_instance.status_description = e.message
+        import_instance.status = processing_status
+        import_instance.status_description = 'Currently processing file'
         import_instance.save()
 
-    return True
+        import_context = import_instance.get_context()
+
+        try:
+            if importer.process(import_context, logger) is True:
+                import_instance.status = success_status
+                import_instance.status_description = success_description
+                import_instance.save()
+
+            else:
+                raise ImportFailure(assuming_failure_message.format(
+                    importer.__class__.__name__
+                ))
+
+        except ImportFailure, e:
+            import_instance.status = failure_status
+            import_instance.status_description = e.message
+            import_instance.save()
+
+        return True
